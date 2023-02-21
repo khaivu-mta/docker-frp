c7a3aba7fffae33cebd02d1152446ce43725e9cf
kv <khaivu.mta@gmail.com>
2023-02-21 21:50:25 +0700

    update dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,25 +1,22 @@
 FROM alpine
 
-LABEL MAINTAINER "ZhangSean <zxf2342@qq.com>"
-
-ENV FRP_VERSION=v0.44.0
+ENV FRP_VERSION=v0.47.0
 
 ADD entrypoint.sh /entrypoint.sh
 
-RUN addgroup -S frp \
- && adduser -D -S -h /var/frp -s /sbin/nologin -G frp frp \
- && apk add --no-cache curl \
- && curl -fSL https://github.com/fatedier/frp/releases/download/${FRP_VERSION}/frp_${FRP_VERSION:1}_linux_amd64.tar.gz -o frp.tar.gz \
+WORKDIR /build
+
+RUN apk add --no-cache curl \
+ && curl -fSL https://github.com/fatedier/frp/releases/download/${FRP_VERSION}/frp_${FRP_VERSION:1}_linux_386.tar.gz -o frp.tar.gz \
  && tar -zxv -f frp.tar.gz \
  && rm -rf frp.tar.gz \
- && mv frp_*_linux_amd64 /frp \
- && chown -R frp:frp /frp \
+ && mkdir /config \
+ && mv frp_*_linux_386 /frp \
+ && mv /frp/*.ini /config \
  && mv /entrypoint.sh /frp/
-
-USER frp
 
 WORKDIR /frp
 
-EXPOSE 6000 7000
+EXPOSE 6000 6001 7000 7001
 
 CMD ["/frp/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -3,38 +3,38 @@ if [ -z $MODE ]; then
   MODE=server
 fi
 if [ $FRP_TOKEN ]; then
-  sed -i "1 a token = $FRP_TOKEN" /frp/frps.ini
-  sed -i "1 a token = $FRP_TOKEN" /frp/frpc.ini
+  sed -i "1 a token = $FRP_TOKEN" /config/frps.ini
+  sed -i "1 a token = $FRP_TOKEN" /config/frpc.ini
 fi
 if [ $MODE = server ]; then
   if [ $BIND_PORT ]; then
-    sed -i "s|bind_port = 7000|bind_port = $BIND_PORT|g" /frp/frps.ini
+    sed -i "s|bind_port = 7000|bind_port = $BIND_PORT|g" /config/frps.ini
   fi
-  /frp/frps -c /frp/frps.ini
+  /frp/frps -c /config/frps.ini
 else
   if [ $SERVER_ADDR ]; then
-    sed -i "s|server_addr = 127.0.0.1|server_addr = $SERVER_ADDR|g" /frp/frpc.ini
+    sed -i "s|server_addr = 127.0.0.1|server_addr = $SERVER_ADDR|g" /config/frpc.ini
   fi
   if [ $FRP_USER ]; then
-    sed -i "1 a user = $FRP_USER" /frp/frpc.ini
+    sed -i "1 a user = $FRP_USER" /config/frpc.ini
   fi
   if [ $PROXY_NAME ]; then
-    sed -i "s|ssh|$PROXY_NAME|g" /frp/frpc.ini
+    sed -i "s|ssh|$PROXY_NAME|g" /config/frpc.ini
   fi
   if [ $SERVER_PORT ]; then
-    sed -i "s|server_port = 7000|server_port = $SERVER_PORT|g" /frp/frpc.ini
+    sed -i "s|server_port = 7000|server_port = $SERVER_PORT|g" /config/frpc.ini
   fi
   if [ $PROTO ]; then
-    sed -i "s|type = tcp|type = $PROTO|g" /frp/frpc.ini
+    sed -i "s|type = tcp|type = $PROTO|g" /config/frpc.ini
   fi
   if [ $LOCAL_IP ]; then
-    sed -i "s|local_ip = 127.0.0.1|local_ip = $LOCAL_IP|g" /frp/frpc.ini
+    sed -i "s|local_ip = 127.0.0.1|local_ip = $LOCAL_IP|g" /config/frpc.ini
   fi
   if [ $LOCAL_PORT ]; then
-    sed -i "s|local_port = 22|local_port = $LOCAL_PORT|g" /frp/frpc.ini
+    sed -i "s|local_port = 22|local_port = $LOCAL_PORT|g" /config/frpc.ini
   fi
   if [ $REMOTE_PORT ]; then
-    sed -i "s|remote_port = 6000|remote_port = $REMOTE_PORT|g" /frp/frpc.ini
+    sed -i "s|remote_port = 6000|remote_port = $REMOTE_PORT|g" /config/frpc.ini
   fi
-  /frp/frpc -c /frp/frpc.ini
+  /frp/frpc -c /config/frpc.ini
 fi
